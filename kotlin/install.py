d67f24dc13731a0d606688af0bef9f84ef823a92
Boris Staletic <boris.staletic@gmail.com>
2019-07-14 15:48:49 +0200

    Kotlin master doesn't send snippets any more

--- kotlin/install.py
+++ kotlin/install.py
@@ -10,8 +10,6 @@ def Main():
   url = 'https://github.com/fwcd/KotlinLanguageServer'
   subprocess.check_call( [ 'git', 'clone', '--depth', '1', url ] )
   os.chdir( 'KotlinLanguageServer' )
-  subprocess.check_call( [ 'git', 'fetch', 'origin', '+refs/pull/120/merge' ] )
-  subprocess.check_call( [ 'git', 'checkout', 'FETCH_HEAD' ] )
   if OnWindows():
     subprocess.check_call( [ './gradlew.bat', 'server:installDist' ] )
   else:
