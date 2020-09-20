eca81c3052e9020449d9032f82e3b96aba7c8de8
Ben Jackson <puremourning@gmail.com>
2020-09-20 13:22:05 +0100

    Add godot

---
+++ godot/install.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+print( "**************************************************************" )
+print( "To enable GoDot language server" )
+print( "**************************************************************" )
+print( "" )
+print( "You must enable the godot language server manually, by setting "
+       "Project -> Project Settings -> Global -> Language Server to ON" )
+print( "" )
+print( "**************************************************************" )
