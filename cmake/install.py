8fe1bb2d5ca95663efefe68da96b37bc41cd5c69
brian <brian.orwe@gmail.com>
2021-04-20 13:35:36 +0300

    Made changes

--- cmake/install.py
+++ cmake/install.py
@@ -1,73 +1,67 @@
 #!/usr/bin/env python3
 import subprocess, platform, sys, os
 
-PYCMD = sys.executable
+PY_CMD = sys.executable
 DIR_OF_THIS_SCRIPT = os.path.dirname( os.path.abspath( __name__ ) )
 
 def OnWindows():
     return platform.system() == 'Windows'
 
 
-def ProcessRun(args):
-    if len(args)>0:
-        try:
-            subprocess.run(args)
-            return True
-        except:
-            return False
-    return False
+def ProcessRun( args ):
+    subprocess.check_call( args )
 
 
-def CheckIfHavePipInstalled():
-    print("Checking for pip")
-    cmd=["pip","--version"]
-    if ProcessRun(cmd):
+def isPipInstalled():
+    print( "Checking for pip" )
+    try:
+        cmd = [ "pip" , "--version" ]
+        ProcessRun( cmd )
         return "pip"
-
-    cmd=["pip3","--version"]
-    if ProcessRun(cmd):
+    except:
+        cmd=[ "pip3" , "--version" ]
+        ProcessRun( cmd )
         return "pip3"
 
     return None
 
 
 def CreateVenvDir():
-    print("Creating venv")
-    cmd=[PYCMD,"-m","venv","venv"]
-    if ProcessRun(cmd):
+    print( "Creating venv" )
+    cmd = [ PY_CMD , "-m" , "venv" , "venv" ]
+    try:
+        ProcessRun( cmd )
         return True
-
-    return False
+    except:
+        return False
 
 
 def InstallCMakeLanguageServer():
-    binDir="bin"
+    bin_dir = "bin"
     if OnWindows():
-        binDir="Scripts"
+        bin_dir = "Scripts"
 
-    pipCMD = os.path.join(DIR_OF_THIS_SCRIPT,"venv",binDir,"pip")
-    cmd=[pipCMD,"install","cmake-language-server"]
-    if ProcessRun(cmd):
+    pip_cmd = os.path.join( DIR_OF_THIS_SCRIPT , "venv" , bin_dir , "pip" )
+    cmd=[ pip_cmd , "install" , "cmake-language-server" ]
+    try:
+        ProcessRun( cmd )
         return True
-    return False
+    except:
+        return False
 
 
 def Main():
-    pipToUse=CheckIfHavePipInstalled()
-    if pipToUse==None:
-        print("Couldn't find pip in your system, please put it on path")
-        return
+    pip_to_use = isPipInstalled()
+    if pip_to_use == None:
+        sys.exit( "Couldn't find pip in your system, please put it on path" )
 
-    createdVenv=CreateVenvDir()
-    if createdVenv==False:
-        print("Error, couldn't create the venv directory to be used")
-        return
+    created_venv = CreateVenvDir()
+    if created_venv == False:
+        sys.exit( "Error, couldn't create the venv directory to be used" )
 
-    installCMake=InstallCMakeLanguageServer()
-    if installCMake==False:
-        print("Error in install process of cmake-language-server")
-        return
-
+    install_cmake = InstallCMakeLanguageServer()
+    if install_cmake == False:
+        sys.exit( "Error in install process of cmake-language-server" )
 
 
 if __name__ == "__main__":
