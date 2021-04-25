95d0ec3c4b2946e1f1ea23aff8f9913341e022fa
brian.orwe <brian.orwe@gmail.com>
2021-04-25 15:03:42 +0300

    Did some corrections

--- cmake/install.py
+++ cmake/install.py
@@ -8,64 +8,40 @@ PY_CMD = sys.executable
 DIR_OF_THIS_SCRIPT = os.path.dirname( os.path.abspath( __name__ ) )
 
 def OnWindows():
-    return platform.system() == 'Windows'
-
-
-def ProcessRun( args ):
-    subprocess.check_call( args )
-
-
-def isPipInstalled():
-    print( "Checking for pip" )
-    try:
-        cmd = [ "pip" , "--version" ]
-        ProcessRun( cmd )
-        return "pip"
-    except:
-        cmd=[ "pip3" , "--version" ]
-        ProcessRun( cmd )
-        return "pip3"
-
-    return None
+  return platform.system() == 'Windows'
 
 
 def CreateVenvDir():
-    print( "Creating venv" )
-    cmd = [ PY_CMD , "-m" , "venv" , "venv" ]
-    try:
-        ProcessRun( cmd )
-        return True
-    except:
-        return False
+  print( "Creating venv" )
+  cmd = [ PY_CMD, "-m", "venv", "venv" ]
+  try:
+    subprocess.check_call( cmd )
+    return True
+  except:
+    return False
 
 
 def InstallCMakeLanguageServer():
-    bin_dir = "bin"
-    if OnWindows():
-        bin_dir = "Scripts"
+  bin_dir = "bin"
+  if OnWindows():
+    bin_dir = "Scripts"
 
-    pip_cmd = os.path.join( DIR_OF_THIS_SCRIPT , "venv" , bin_dir , "pip" )
-    cmd=[ pip_cmd , "install" , "cmake-language-server" ]
-    try:
-        ProcessRun( cmd )
-        return True
-    except:
-        return False
+  pip_cmd = os.path.join( DIR_OF_THIS_SCRIPT, "venv", bin_dir, "pip" )
+  cmd = [ pip_cmd, "install", "cmake-language-server" ]
+  try:
+    subprocess.check_call( cmd )
+    return True
+  except:
+    return False
 
 
 def Main():
-    pip_to_use = isPipInstalled()
-    if pip_to_use == None:
-        sys.exit( "Couldn't find pip in your system, please put it on path" )
+  if not CreateVenvDir():
+    sys.exit( "Error, couldn't create the venv directory to be used" )
 
-    created_venv = CreateVenvDir()
-    if created_venv == False:
-        sys.exit( "Error, couldn't create the venv directory to be used" )
-
-    install_cmake = InstallCMakeLanguageServer()
-    if install_cmake == False:
-        sys.exit( "Error in install process of cmake-language-server" )
+  if not InstallCMakeLanguageServer():
+    sys.exit( "Error in install process of cmake-language-server" )
 
 
 if __name__ == "__main__":
-    Main()
+  Main()
