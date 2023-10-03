e9b75faffca0b73482e95ca92f16cdf382390987
Kamen Vakavchiev <kanitkameh@gmail.com>
2023-10-03 21:43:34 +0300

    add racket lsp config for ycm

---
+++ racket/install.py
@@ -0,0 +1,58 @@
+import subprocess, os, platform, sys
+
+def OnWindows():
+  return platform.system() == 'Windows'
+
+
+# On Windows, distutils.spawn.find_executable only works for .exe files
+# but .bat and .cmd files are also executables, so we use our own
+# implementation.
+def FindExecutable( executable ):
+  # Executable extensions used on Windows
+  WIN_EXECUTABLE_EXTS = [ '.exe', '.bat', '.cmd' ]
+
+  paths = os.environ[ 'PATH' ].split( os.pathsep )
+  base, extension = os.path.splitext( executable )
+
+  if OnWindows() and extension.lower() not in WIN_EXECUTABLE_EXTS:
+    extensions = WIN_EXECUTABLE_EXTS
+  else:
+    extensions = [ '' ]
+
+  for extension in extensions:
+    executable_name = executable + extension
+    if not os.path.isfile( executable_name ):
+      for path in paths:
+        if path.startswith('~'):
+          path = os.path.expanduser('~')+path[1:]
+
+        executable_path = os.path.join( path, executable_name )
+        if os.path.isfile( executable_path ):
+          return executable_path
+    else:
+      return executable_name
+  return None
+
+
+def FindExecutableOrDie( executable, message ):
+  path = FindExecutable( executable )
+
+  if not path:
+    sys.exit( "ERROR: Unable to find executable '{0}'. {1}".format(
+      executable,
+      message ) )
+
+  return path
+
+
+def Main():
+  git = FindExecutableOrDie('raco', 'raco is required to setup Racket language server')
+
+  print("Installing racket-langserver and fmt from raco")
+  os.system("raco pkg install racket-langserver")
+  # Used by Vim when it recognizes racket filetype 
+  os.system("raco pkg install fmt")
+
+
+if __name__ == '__main__':
+  Main()
---
+++ racket/snippet.vim
@@ -0,0 +1,6 @@
+let g:ycm_language_server += [
+  \   { 'name': 'racket',
+  \     'filetypes': [ 'racket' , 'scheme' ],
+  \     'cmdline': [ 'racket', '-l', 'racket-langserver' ],
+  \   },
+  \ ]
